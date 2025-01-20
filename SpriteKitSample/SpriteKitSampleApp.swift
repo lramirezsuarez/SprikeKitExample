4b9c59cf8e1c7d742594e2ac5a4e3a063bfa8fb6
Luis Ramirez <lramirezsuarez@gmail.com>
2025-01-20 17:52:21 -0500

    Initial Commit

---
+++ SpriteKitSample/SpriteKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitSampleApp.swift
+//  SpriteKitSample
+//
+//  Created by Luis Alejandro Ramirez Suarez on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
